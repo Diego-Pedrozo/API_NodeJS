0aa7054afdcf88e655108ed7b62606a1d95a9be7
Diego-Pedrozo <diegopedrozovillarreal@gmail.com>
2023-11-12 17:42:37 -0500

    Ajustes del manejo de errores

--- src/index.js
+++ src/index.js
@@ -7,5 +7,9 @@ app.use(express.json());
 //routes
 app.use(require('./routes/openai'));
 
+app.get('/', (req, res) => {
+    res.send('API AlexaSkill')
+})
+
 const port = process.env.port || 8080;
-app.listen(port, () => console.log(`http://localhost:8080/api/`));
+app.listen(port, () => console.log(`http://localhost:8080/`));
--- src/routes/openai.js
+++ src/routes/openai.js
@@ -11,14 +11,8 @@ const openai = new OpenAIApi(configuration);
 
 const responses = {};
 
-router.post('/api/chatgpt', async (req, res) => {
+const openAIRequest = async (userId, question) => {
     try {
-        let message = { message: 'Esperando' }
-        res.json(message);
-        //res.send("Esperando respuesta de chatgpt...")
-        let userId = req.body.userId;
-        let question = req.body.question;
-        responses[userId] = { completion: null, state: 0 }
         const completion = await openai.createCompletion({
             model: "text-davinci-003",
             prompt: question,
@@ -30,12 +24,60 @@ router.post('/api/chatgpt', async (req, res) => {
         });
         responses[userId] = { completion, state: 1 };
     } catch (error) {
-        console.error(error);
-        let message = { message: 'Error al consumir la API de OPENAI' }
-        res.status(500).json(message);
-        //res.status(500).send('Error al consumir la API de OPENAI');
+        throw error;
+    }
+}
+
+router.post('/api/chatgpt', async (req, res) => {
+    try {
+        let userId = req.body.userId;
+        let question = req.body.question;
+        responses[userId] = { completion: null, state: 0 };
+        // let message = { message: 'Esperando' }
+        // res.json(message);
+        await openAIRequest(userId, question);
+    } catch (error) {
+        console.error(error.response.data);
+        let userId = req.body.userId;
+        responses[userId] = { completion: 'Error al consumir la API de OPENAI', state: 2 };
+        //delete responses[userId];
+        let message = {
+            message: 'Error al consumir la API de OPENAI',
+            error: error.response.data.error.message
+        }
+        res.status(error.response.status).json(message);
     }
 });
+
+// router.post('/api/chatgpt', async (req, res) => {
+//     try {
+//         let message = { message: 'Esperando' }
+//         //res.json(message);
+//         //res.send("Esperando respuesta de chatgpt...")
+//         let userId = req.body.userId;
+//         let question = req.body.question;
+//         responses[userId] = { completion: null, state: 0 }
+//         const completion = await openai.createCompletion({
+//             model: "text-davinci-003",
+//             prompt: question,
+//             temperature: 0,
+//             max_tokens: 1500,
+//             top_p: 1,
+//             frequency_penalty: 0.0,
+//             presence_penalty: 0.0,
+//         });
+//         responses[userId] = { completion, state: 1 };
+//     } catch (error) {
+//         console.error(error.response.data);
+//         responses[userId] = { completion: null, state: 2 }
+//         let message = {
+//             message: 'Error al consumir la API de OPENAI',
+//             error: error.response.data.error.message
+//         }
+//         res.status(error.response.status).json(message);
+//         //res.status(500).send('Error al consumir la API de OPENAI');
+//     }
+// });
 
 router.get('/api/getResponse/:userId', async (req, res) => {
     try {
@@ -47,14 +89,22 @@ router.get('/api/getResponse/:userId', async (req, res) => {
             message = { userId, estado, message: 'Todavia no has realizado ninguna pregunta' }
             res.json(message);
             //res.send('Todavia no has realizado ninguna pregunta')
-        } else if (message.estado.state != 1) {
+        } else if (message.estado.state == 0) {
             message = { userId, estado: estado.state, message: 'Espera' }
             res.json(message);
             //res.send('Trabajando en tu respuesta')
         }
-        else {
+        else if (message.estado.state == 1){
             //let response = responses[userId];
             message = { userId, estado: estado.state, message: estado.completion.data.choices[0].text }
+            res.json(message);
+            //res.send(response.completion.data.choices[0].text);
+            //responses[userId] = null;
+            delete responses[userId];
+        }
+        else if (message.estado.state == 2){
+            //let response = responses[userId];
+            message = { userId, estado: estado.state, message: estado.completion }
             res.json(message);
             //res.send(response.completion.data.choices[0].text);
             //responses[userId] = null;
